3bc5447322cd9f3c1b7ac666a1ae6d95367a2e45
Goldie323 <goldieross323@gmail.com>
2025-07-13 00:55:23 -0400

    removed files that are system specific and built these files where only there for me to run the program and they shouldn't have been in the repo in the first place

--- Library-Code/build/CMakeFiles/MCServerHandler.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MCServerHandler.
-# This may be replaced when dependencies are built.
--- Main.py
+++
@@ -1,6 +0,0 @@
-from ctypes import cdll
-
-mylib = cdll.LoadLibrary("./Library-Code/build/libMCServerHandler.so")
-
-d = mylib.math_add(5, 3)
-print(f"Result of addition: {d}")
